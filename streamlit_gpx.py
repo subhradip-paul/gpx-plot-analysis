575990ae5c17ea9f82e8e2635c81140eeb58ab47
Subhradip Paul <subhradip.paul.sp@gmail.com>
2024-11-19 22:21:16 +0100

    building app still

--- streamlit_gpx.py
+++ streamlit_gpx.py
@@ -1,64 +1,87 @@
+#%% Header files
 import streamlit as st
 from gpxpy import parse
+from gpxpy.geo import distance
+
 import pandas as pd
 import folium
-from streamlit_folium import st_folium, folium_static
+from streamlit_folium import st_folium
+import plotly.express as px
+import numpy as np
+import os
+
+def file_selector(folder_path=r"./gpx_files/Alta Via 2 - Our Way/"):
+    filenames = os.listdir(folder_path)
+    selected_filename = st.selectbox('Select a file', filenames)
+    return os.path.join(folder_path, selected_filename)
 
 
-keys=['lat', 'lon', 'elev', 'time']
+keys=['lat', 'lon', 'elev', 'time', 'speed', 'dist3d']
 dict_gpx = {}
 for key in keys:
     dict_gpx[key] = []
 
-file = st.file_uploader("Upload waypoinst file: ",type=["gpx"])
+file = file_selector()
+st.write(f'You selected {file}')
+
+
 if file:
-    gpx = parse(file) # type: ignore
-    for track in gpx.tracks:
+    gpxfile = parse(file)# type: ignore
+    sum_lat = 0.
+    sum_lon = 0.
+    n=0.
+    for track in gpxfile.tracks:
         for segment in track.segments:
-            for point in segment.points:
+            for idx, point in enumerate(segment.points):
                 dict_gpx['lat'].append(point.latitude)
                 dict_gpx['lon'].append(point.longitude)
                 dict_gpx['elev'].append(point.elevation)
                 dict_gpx['time'].append(point.time)
+                dict_gpx['speed'].append(point.speed)
+                if idx==0:
+                    start_lat=point.latitude
+                    start_lon=point.longitude
+                    start_elev=point.elevation
+                    dict_gpx['dist3d'].append(0.0)
+                    dict_gpx['gradient'].append(0.0)
+                if idx:
+                    dist3d=distance(point.latitude,point.longitude,point.elevation,start_lat,start_lon,start_elev)
+                    dict_gpx['dist3d'].append(dist3d)
+                    
+                sum_lat += point.latitude
+                sum_lon += point.longitude
+                n +=1                
+    st.write(":arrow_up:", track.get_uphill_downhill().uphill)
+    # print(track.get_duration()) 
+           
     df_gpx=pd.DataFrame.from_dict(dict_gpx)
-    start_lat = df_gpx.loc[0,'lat']
-    start_lon = df_gpx.loc[0,'lon']
-    end_lat = df_gpx['lat'].iloc[-1]
-    end_lon = df_gpx['lon'].iloc[-1]
-    m = folium.Map(location=[(start_lat + end_lat)/2, (start_lon + end_lon)/2], zoom_start=16)
+    
+    max_lat = df_gpx['lat'].max
+    min_lat = df_gpx['lon'].max
+    
+    
+    m = folium.Map(location=[sum_lat/n, sum_lon/n], min_lat=min_lat, max_lat=max_lat, zoom_start=12)
+    folium.TileLayer("HikeBike.HillShading").add_to(m)
+    folium.TileLayer(show=False).add_to(m)
+    folium.LayerControl().add_to(m)
     coordinates = [tuple(x) for x in df_gpx[['lat','lon']].to_numpy()]
-    folium.PolyLine(coordinates, weight=3).add_to(m)
-    folium_static(m)    
-
-
-
-
-
-#   folium.Marker(     [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
-# ).add_to(m)
-                
-
-# m = folium.Map(location=[(start_lat + end_lat)/2, (start_lon + end_lon)/2], zoom_start=16)
-# st_data = st_folium(m, width=725)
-
-
-# route_map = folium.Map(
-#     location=[latitude,longitude] ,
-#     zoom_start=14 ,
-#     tiles='CartoDBPositron' )
-
-# coordinates = [tuple(x) for x in 
-#  df[['latitude','longitude']].to_numpy()]
-
-# folium.PolyLine(coordinates ,
-#  weight=3).add_to(route_map)
-
-# center on Liberty Bell, add marker
-# m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
-# folium.Marker(
-#     [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
-# ).add_to(m)
-
-
-
-# st.map(df_gpx, size='0.1')
+    folium.PolyLine(coordinates, weight=2).add_to(m)
+    st_folium(m, width=725)
+    fig_elev_time = px.line(
+    df_gpx,
+    x="time",
+    y="elev",
+    hover_data=["elev"],
+    )
+    
+    fig_elev_dist = px.line(
+    df_gpx,
+    x="dist3d",
+    y="elev",
+    hover_data=["elev"],
+    )
+    
+    
+    
+    st.plotly_chart(fig_elev_time, use_container_width=True)
+    st.plotly_chart(fig_elev_dist, use_container_width=True)
